508bbde7d84153bb60672390e3d323adc52bcb6f
Garrett <garrett@garrettboast.com>
2017-06-28 15:40:46 -0400

    eol conversion

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-mc config host add minio $MINIO_HOST $ACCESS_KEY $SECRET_KEY S3v4
+MINIO_HOST=${MINIO_HOST:-"https://play.minio.io:9000/"}
+ACCESS_KEY=${ACCESS_KEY:-"Q3AM3UQ867SPQQA43P2F"}
+SECRET_KEY=${SECRET_KEY:-"zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"}
 
+mc config host add minio $MINIO_HOST $ACCESS_KEY $SECRET_KEY S3v4
