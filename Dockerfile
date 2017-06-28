0b257cae4b67590620131b36f594e1d511ca5b52
Garrett <garrett@garrettboast.com>
2017-06-28 15:50:32 -0400

    Destination folder/root; copy process

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM minio/mc
 MAINTAINER garrett@garrettboast.com
 
 COPY entrypoint.sh /bin/entrypoint.sh
-RUN ln -s /bin/entrypoint.sh /entrypoint.sh
-RUN chmod a=rx /bin/entrypoint.sh
-
+RUN ln -s /bin/entrypoint.sh /entrypoint.sh \
+	&& chmod a=rx /bin/entrypoint.sh \
+	&& mkdir -p /deploy/
 
 ENTRYPOINT ["entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,10 @@
 MINIO_HOST=${MINIO_HOST:-"https://play.minio.io:9000/"}
 ACCESS_KEY=${ACCESS_KEY:-"Q3AM3UQ867SPQQA43P2F"}
 SECRET_KEY=${SECRET_KEY:-"zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"}
+SOURCE_BUCKET=${SOURCE_BUCKET:-"my-test-bucket"}
+DEST_ROOT=${DEST_ROOT:-"/deploy"}
+DEST_FOLDER="$DEST_ROOT/${DEST_FOLDER:-'src'}"
 
+mkdir -p "$DEST_FOLDER"
 mc config host add minio $MINIO_HOST $ACCESS_KEY $SECRET_KEY S3v4
+mc cp --recursive "minio/$SOURCE_BUCKET" "$DEST_FOLDER"
